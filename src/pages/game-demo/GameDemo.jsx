50b1e0999de636fe78177b31797f476efee4b849
bgusenda <pepcoelho@proton.me>
2025-11-19 12:56:43 -0300

    almost done

--- src/pages/game-demo/GameDemo.jsx
+++ src/pages/game-demo/GameDemo.jsx
@@ -1,9 +1,15 @@
 export default function GameDemoPage() {
     return (
-        <>
-        
-        <h1>BAZINGA</h1>
-        
-        </>
+        <div className="container jc-center ai-center" style={{ padding: "6em 0"}}>
+            <iframe
+                src="/games/index.html"
+                width="800"
+                height="600"
+                style={{
+                    border: 'none',
+                    boxShadow: '0 0 20px rgba(247, 247, 247, 0.66)',
+                }}
+            ></iframe>
+        </div>
     )
 }
