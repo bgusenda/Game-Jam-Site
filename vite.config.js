04755a17a9e4e849acea7ce630414123b23a537c
bgusenda <pepcoelho@proton.me>
2025-11-19 11:30:30 -0300

    vercel attempt 1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import ghPages from "vite-plugin-gh-pages";
-
-const repoName = "Game-Jam-Site";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: `/${repoName}/`,
+  plugins: [react()],
 });
